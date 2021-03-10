2c3d8e10046b43e13eee64886f8b4436dfbb653f
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2021-03-10 17:10:31 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import * as core from "@actions/core";
 import { getIssueContent } from "./getIssueContent";
 import { checkKeywords } from "./checkKeywords";
-import { setIssueLabel } from "./setIssueLabel";
 import { setIssueAssignee } from "./setIssueAssignee";
 
 async function run() {
   try {
-    core.setOutput("labeled", false.toString());
     core.setOutput("assigned", false.toString());
     const titleOrBody: string = core.getInput("title-or-body");
     const token = core.getInput("github-token");
     const content = await getIssueContent(token, titleOrBody);
-    const parameters: { keywords: string[], labels: string[], assignees: string[] }[] = JSON.parse(
+    const parameters: {assignees: string[] }[] = JSON.parse(
       core.getInput("parameters", {required: true})
     );
     if (!parameters) {
       core.setFailed(
         `parameters input not found. Make sure your ".yml" file contains a "parameters" JSON array like this:
-        parameters: '[ {"keywords": ["bug", "error"], "labels": ["BUG"], "assignees": ["username"]}, {"keywords": ["help", "guidance"], "labels": ["help-wanted"], "assignees": ["username"]}]'`
+        parameters: '[ {"assignees": ["username"]}]`
       );
     }
 
@@ -27,9 +25,6 @@ async function run() {
       console.log("Keywords not included in this issue");
       return;
     } else {
-      setIssueLabel(token, matchingKeywords);
-      core.setOutput("labeled", true.toString());
-  
       setIssueAssignee(token, matchingKeywords);
       core.setOutput("assigned", true.toString());
     }
